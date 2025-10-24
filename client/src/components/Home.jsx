aab9478808017404a3b56ef04383007b39e6cf5d
Kasuni Kariyawasam <kmkariyawasam4@gmail.com>
2025-10-24 18:48:56 +0530

    Create Home Page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../css/home.css'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='hero'>
+            <div className='hero-content'>
+                <h1 className='hero-text'>Book Shop</h1>
+                <p className='hero-description'>
+                    Browse the collection of our best top interesting Books.
+                    You will definitely find what you are looking for.
+                </p>
+            </div>
+            <div className='hero-image'></div>
+        </div>
     )
 }
 
