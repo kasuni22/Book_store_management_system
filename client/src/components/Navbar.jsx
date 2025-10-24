850bd24f77504a18d9e1ad01bafbcd5f328f07b6
Kasuni Kariyawasam <kmkariyawasam4@gmail.com>
2025-10-24 15:53:57 +0530

    create Navigation bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className='navbar'>
+            <div className='navbar-left'>
+                <Link to="/" className='navbar-brand'>Book Store</Link>
+            </div>
+            <div className='navbar-right'>
+                <Link to="/books" className='navbar-link'>Books</Link>
+                <Link to="/login" className='navbar-link'>Login</Link>
+            </div>
+        </nav>
+        
     )
 }
 
